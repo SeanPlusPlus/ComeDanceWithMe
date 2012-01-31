2e84de69afa01381401ad66520a061673aa81731
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-01-30 20:14:43 -0800

    major clean up of directory structure

--- config.ru
+++
@@ -1,2 +0,0 @@
-require './expense.rb'
-run Sinatra::Application
---
+++ models/Post.rb
@@ -0,0 +1,14 @@
+# /models/Expense.rb
+
+require 'dm-core'
+require 'dm-migrations'
+
+#DataMapper.setup( :default, "sqlite3:///Users/sms/Projects/ComeDanceWithMe/db/database.db")
+
+class Expense
+  include DataMapper::Resource
+
+  property :id,          Serial # Auto-increment integer key
+  property :title,       String
+  property :description, String
+end
